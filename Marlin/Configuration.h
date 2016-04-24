5197f5c8ed752cb3900386109de2acdce1f8661c
Victor Antonovich <v.antonovich@gmail.com>
2016-04-24 12:35:24 +0300

    Changed Z axis params

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -435,9 +435,9 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
   // these are the offsets to the probe relative to the extruder tip (Hotend - Probe)
   #define X_PROBE_OFFSET_FROM_EXTRUDER 30.00
   #define Y_PROBE_OFFSET_FROM_EXTRUDER 1.80
-  #define Z_PROBE_OFFSET_FROM_EXTRUDER -5.35
+  #define Z_PROBE_OFFSET_FROM_EXTRUDER -5.40
 
-  #define Z_RAISE_BEFORE_HOMING 5       // (in mm) Raise Z before homing (G28) for Probe Clearance.
+  #define Z_RAISE_BEFORE_HOMING 10       // (in mm) Raise Z before homing (G28) for Probe Clearance.
                                         // Be sure you have this distance over your Z_MAX_POS in case
 
   #define XY_TRAVEL_SPEED 6000         // X and Y axis travel speed between probes, in mm/min
